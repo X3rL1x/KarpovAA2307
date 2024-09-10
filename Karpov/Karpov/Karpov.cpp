ae9eac4f43276464891ee4c85e95d80a2b1ac5bb
X3rL1x <nikitkacherkas836350@gmail.com>
2024-09-10 12:32:21 +0300

    123

---
+++ Karpov/Karpov/Karpov.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int karp;
+    cout << "Hello World!\n";
+    cout << "abcd" << endl;
+    cin >> karp;
+    cout << karp << endl;
+    cin.get();
+}
+
+
